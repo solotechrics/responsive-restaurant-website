ce80f82f2587b45d0359d03252df7c2441c694a6
Solomon Opeyemi <solotechrics@gmail.com>
2025-11-19 08:24:18 +0100

    Create a responsisve navbar for the webpage

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('mobile-menu');
+
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+})
