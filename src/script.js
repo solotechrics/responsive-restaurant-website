1128ad6e1c3f5f4b72971913d4ea8fa83c3cb531
Solomon Opeyemi <solotechrics@gmail.com>
2025-11-19 10:27:33 +0100

    Add responsiveness to navbar for each breakpoints

--- src/script.js
+++ src/script.js
@@ -1,7 +1,18 @@
-const btn = document.getElementById('menu-btn');
-const menu = document.getElementById('mobile-menu');
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const closeIcon = document.getElementById("close-icon");
 
+menuBtn.addEventListener("click", () => {
 
-btn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-})
+  // Show/Hide mobile menu
+  mobileMenu.classList.toggle("hidden");
+
+  // Toggle icons visibility
+  hamburgerIcon.classList.toggle("opacity-0");
+  closeIcon.classList.toggle("opacity-0");
+
+  // Rotate animation
+  hamburgerIcon.classList.toggle("rotate-90");
+  closeIcon.classList.toggle("rotate-90");
+});
